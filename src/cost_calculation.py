808a18d27c38583d7813a6efbb01f5f372583528
Yannis BORDJI <bordji.yannis@gmail.com>
2026-09-16 11:37:08 +0200

    MAJ de l'interface et du pipeline

--- src/cost_calculation.py
+++ src/cost_calculation.py
@@ -3,13 +3,14 @@ import duckdb
 import api
 
 class Calculations:
-    def __init__(self, code_postal, predi):
+    def __init__(self, code_postal, predi, carburant):
         # On attache les variables à l'instance courante avec self
         self.code_p = str(code_postal)
         #self.api_ = api.Donnees_gouv()
         self.api = predi
         self.con = duckdb.connect()
         self.con.execute("INSTALL spatial; LOAD spatial;")
+        self.carbu = carburant
 
     def in_radius(self, radius):
         # On trouve les coordonnées à partir du code postal
@@ -31,16 +32,30 @@ class Calculations:
         rayon_metres = radius * 1000
 
         liste_stations = pd.DataFrame(data=self.api)
+
         resultats = self.con.execute(f"""
         SELECT 
-            *
+            'https://www.google.com/maps/search/?api=1&query=' || 
+        url_encode(concat_ws(' ', adresse, ville)) AS maps,
+            carburant,
+            date,
+            prix_actuel,
+            prix_predit_demain AS Prix_predit_demain,
+            tendance,
+            variation_predite_euros AS Variation,
+            cp AS Code_postal,
+            ville,
+            adresse
         FROM liste_stations
         WHERE latitude IS NOT NULL AND longitude IS NOT NULL 
         AND ST_Distance_Sphere(
                     ST_Point(longitude, latitude), 
                     ST_Point({lon}, {lat})
       ) <= {rayon_metres} 
+        AND carburant = '{self.carbu}'
+        ORDER BY prix_actuel
         """)
 
+        #
         return resultats
 
--- src/interface.py
+++ src/interface.py
@@ -3,6 +3,8 @@ import cost_calculation as cc
 import re
 from st_files_connection import FilesConnection
 
+st.set_page_config(layout="wide")
+
 conn = st.connection('s3', type=FilesConnection)
 df = conn.read("s3://eco-essence-bordji/predictions/predictions_du_jour.parquet", input_format="parquet", ttl=600)
 
@@ -12,9 +14,26 @@ code_postal = st.text_input("Votre code postal", "69300")
 
 rayon = st.slider("Dans quel rayon recherchez-vous ?",2,100)
 
+carburant_selec = st.radio(
+    "Votre carburant 🛢️:",
+    ["E10","E85","SP98", "Gazole", "GPLc"])
+
 st.divider()
-data = cc.Calculations(code_postal,df)
+data = cc.Calculations(code_postal,df,carburant_selec)
+print(f"Carburant sélectionné : {carburant_selec}")
 pattern = r"^\d{5}$"
 if(bool(re.fullmatch(pattern, code_postal))):
-    st.dataframe(data.in_radius(rayon))
+    resultats =data.in_radius(rayon)
+
+    st.dataframe(
+    resultats,
+    column_config={
+        "maps": st.column_config.LinkColumn(
+            "Itinéraire",
+            display_text="Ouvrir dans Maps",
+            pinned=True
+        )
+    },
+    hide_index=True
+)
     
